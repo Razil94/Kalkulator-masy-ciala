9d1a46e254cc5484212bb4155afb55f53060badb
ST32-217\Student <ryszard.kuczowic@chorzow.merito.pl>
2023-11-26 15:59:10 +0100

    Dodaj pliki projektów.

---
+++ Kalkulator masy ciala/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class BMI
+{
+    static void Main(string[] args)
+    {
+        double a, b, c;
+        Console.Write("Wpisz swoja wage (kg): ");
+        a = double.Parse(Console.ReadLine());
+        Console.Write("Wpisz swoj wzrost (m): ");
+        b = double.Parse(Console.ReadLine());
+        c = (a / (b*b));
+        Console.WriteLine("Twoje BMI : {0}", c);
+      
+        Console.ReadLine();
+
+    }
+}
